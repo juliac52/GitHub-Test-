b847306398c20e5b0c929387eeb4853e9fc2186a
Julia Cox <juliacox@Julias-MacBook-Pro-3.local>
2020-06-08 17:28:16 -0400

    added info about plotting

--- testing 1.R
+++ testing 1.R
@@ -23,8 +23,31 @@ data_long <- gather(Data, Taxa, Count, ctyp_100m3:chaeto_100m3, factor_key=TRUE)
 #view new long dataset
 View(data_long) #having the data in long format will make it much easier to make plots 
 
-#make a plot using ggplot 
+#make a scatterplot using ggplot 
 install.packages('ggplot2')
 library(ggplot2)
+ggplot(data_long, aes(x=date, y=Count, col =Taxa)) + geom_point()
 
+#make a histogram 
+ggplot(data_long, aes(x=date, y=Count, col = Taxa, fill = Taxa)) + geom_bar(stat = 'identity')
 
+#show taxa distribution 
+ggplot(data_long, aes(x=Taxa, y=Count, col = Taxa, fill = Taxa)) + geom_bar(stat = 'identity')
+
+#name plot for cleaner workspace 
+barplot <- ggplot(data_long, aes(x=Taxa, y=Count, col = Taxa, fill = Taxa)) + geom_bar(stat = 'identity')
+
+#can change the title and labels 
+bp1 <- barplot + ggtitle("Zooplankton Taxa Abundance", subtitle="From the EcoMon Dataset") + xlab("Taxa") + ylab("Zooplankton Count")
+
+#can change the colors in your plot 
+#install r color palettes 
+install.packages('RColorBrewer')
+library(RColorBrewer)
+head(brewer.pal.info)
+#change color palette
+bp1 + scale_colour_brewer(palette = "BrBG")
+#more fun colors: http://sape.inf.usi.ch/quick-reference/ggplot2/colour 
+
+#change the theme 
+bp1 + theme_minimal() 
